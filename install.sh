b3e5f11591854bf3182d22732c04ba5ea9bde50b
zawa1120 <ckz1120@gmail.com>
2022-08-11 23:08:16 +0900

    add&fixed

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/zsh
 
 # Install Pakages
+brew install google-chrome --cask
 brew install visual-studio-code --cask
 brew install docker --cask
-brew install google-chrome --cask
+brew install google-cloud-sdk --cask
 
 # Install extension for VScode
 code --install-extension ms-python.python
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
